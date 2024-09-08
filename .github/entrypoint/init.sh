ef1631fbc0103a8a6035148f1d05636e092964cf
eq19 <eq19@users.noreply.github.com>
2024-09-08 01:51:07 +0000

    Assign eQuantum

---
+++ .github/entrypoint/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Structure: Cell Types – Modulo 6
+# https://www.hexspin.com/proof-of-confinement/
+
+hr='------------------------------------------------------------------------------------'
+
+echo -e "\n$hr\nENVIRONTMENT\n$hr"
+printenv | sort
+
+echo -e "\n$hr\nGITHUB CONTEXT\n$hr"
