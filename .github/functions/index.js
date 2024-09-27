211423560323acadd84a86ab10e78f2ea4847a98
eq19 <eq19@users.noreply.github.com>
2024-09-27 07:34:27 +0000

    Update action.yml

--- .github/functions/index.js
+++ .github/functions/index.js
@@ -1,12 +1,25 @@
-import express from 'express';
-const app = express();
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
-});
+'use strict';
 
-const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
+// [START functions_helloworld_get]
+const functions = require('@google-cloud/functions-framework');
+
+// Register an HTTP function with the Functions Framework that will be executed
+// when you make an HTTP request to the deployed function's endpoint.
+functions.http('helloGET', (req, res) => {
+  res.send('Hello World!');
 });
+// [END functions_helloworld_get]
