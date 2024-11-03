47f75003f385ba763c43df00d41846f29869b71b
eq19 <eq19@users.noreply.github.com>
2024-11-03 08:21:06 +0000

    Update action.yml

--- .github/entrypoint/config.sh
+++ .github/entrypoint/config.sh
@@ -1,22 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-setup_config() {
-	# pinned repos
-	# https://stackoverflow.com/a/43358500/4058484
-	curl -s -X POST "${GITHUB_GRAPHQL_URL}" -H "Authorization: bearer $TOKEN" --data-raw '{"query":"{\n  user(login: \"'${GITHUB_REPOSITORY_OWNER}'\") {\n pinnedItems(first: 6, types: REPOSITORY) {\n nodes {\n ... on Repository {\n name\n }\n }\n }\n }\n}"' | jq --raw-output '.data.user.pinnedItems' | yq eval -P | sed "s/name: //g" > nodes.yaml
+git config --global user.name "${{ github.actor }}"
+git config --global user.email "${{ github.actor }}@users.noreply.github.com"
 
-	for i in 0 1 2 3 4 5
-	do
-	   j=$(($i+1))
-	   NAME=$(yq eval ".nodes[$i]" nodes.yaml)
-	   [ -z "${GITHUB_REPOSITORY##*$NAME*}" ] && TARGET=$(yq eval ".nodes[$j]" nodes.yaml)
-	done
+git config --global core.autocrlf false
+git config --global --add safe.directory "${{ github.workspace }}"       
 
-	rm nodes.yaml
-	rm -Rf -- */ && mv /maps/text/_* .
-	REPOSITORY=${GITHUB_REPOSITORY_OWNER}/${TARGET}
-	[ "${REPOSITORY}" == "eq19/eq19.github.io" ] && mv /maps/assets .
-
-	sed -i "1s|^|target_repository: $REPOSITORY\n|" ${JEKYLL_CFG}
-	sed -i "1s|^|repository: $GITHUB_REPOSITORY\n|" ${JEKYLL_CFG}
-}
+echo 'LATEST_COMMIT='$(curl -s "https://api.github.com/users/eq19/events/public" | jq ".[0].payload.commits[0].message") > ${GITHUB_ENV}
+echo 'REMOTE_REPO=https://${{ github.actor }}:${TOKEN}@github.com/${{ github.repository }}.git' > ${GITHUB_ENV}
