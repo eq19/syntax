84809db625b8407cb2f708b2952c1cffa0f67ff4
eq19 <eq19@users.noreply.github.com>
2024-11-03 09:45:26 +0000

    Update main.yml

--- .github/entrypoint/config.sh
+++ .github/entrypoint/config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-git config --global user.name "${{ github.actor }}"
-git config --global user.email "${{ github.actor }}@users.noreply.github.com"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global --add safe.directory "${GITHUB_WORKSPACE}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-git config --global core.autocrlf false
-git config --global --add safe.directory "${{ github.workspace }}"       
+#git config --global credential.helper store
+#echo "https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com" > ~/.git-credentials
 
-echo 'LATEST_COMMIT='$(curl -s "https://api.github.com/users/eq19/events/public" | jq ".[0].payload.commits[0].message") > ${GITHUB_ENV}
-echo 'REMOTE_REPO=https://${{ github.actor }}:${TOKEN}@github.com/${{ github.repository }}.git' > ${GITHUB_ENV}
+echo 'REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"' >> ${GITHUB_ENV}
+echo 'LATEST_COMMIT='$(curl -s "https://api.github.com/users/eq19/events/public" | jq ".[0].payload.commits[0].message") >> ${GITHUB_ENV}
