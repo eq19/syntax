f9caa1c2bfbdce9f6fbad37c98a2e5539dae2e48
eq19 <eq19@users.noreply.github.com>
2024-11-12 13:16:27 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -19,7 +19,7 @@ REMOTE_REPO="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}
 
 API_URL="https://api.github.com/users/eq19/events/public"
 LATEST_COMMIT=$(curl -s $API_URL | jq ".[0].payload.commits[0].message")
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   if [[ -z "$LATEST_COMMIT" ]]; then
     echo 'LATEST_COMMIT="update by workspace"' >> ${GITHUB_ENV}
   elif [[ "$LATEST_COMMIT" == null ]]; then
