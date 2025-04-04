e92cb7d6c5725f5b89ea3d210813752251d2da8e
eq19 <eq19@users.noreply.github.com>
2025-04-04 02:36:23 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -78,8 +78,8 @@ if [[ "${JOBS_ID}" == "1" ]]; then
     fi
 
     if [[ "${RERUN_RUNNER}" != "false" ]]; then gh variable set RERUN_RUNNER --body "false"; fi
-    cd $1 && javac -d user_data/ft_client/test_client javaCode/Main.java
-    cd ${GITHUB_WORKSPACE} && mv -f $1/user_data . && ls -al . 
+    cd $1 && javac -d user_data/ft_client/test_client javaCode/Main.java && cd ${GITHUB_WORKSPACE}
+    if [[ ! -d user_data ]]; then mv -f $1/user_data . && ls -al; fi
 
   fi
 
