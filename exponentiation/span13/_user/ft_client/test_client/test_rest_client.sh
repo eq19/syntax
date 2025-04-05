dbe8291e4424f516f3ab2045439f923872eff80b
eq19 <eq19@users.noreply.github.com>
2025-04-05 17:08:01 +0000

    Update test_rest_client.sh

--- exponentiation/span13/_user/ft_client/test_client/test_rest_client.sh
+++ exponentiation/span13/_user/ft_client/test_client/test_rest_client.sh
@@ -192,22 +192,16 @@ else
   NEW_SCORE=$SCORE
   echo "NEW SCORE: $NEW_SCORE"
 
-  #if (( $(echo "$NEW_SCORE > $OLD_SCORE" | bc -l) )); then
-    #PARAMS=.github/entrypoint/artifact/python/src/params/spaces.json
-    #git clone https://eq19:$GH_TOKEN@github.com/eq19/eq19.git /tmp/eq19
-    #cat $STRATEGY > /tmp/eq19/$PARAMS
-    #gh variable set RERUN_RUNNER --body "true"
-
-    #cd /tmp/eq19
-    #git config --global user.name eq19
-    #git config --global user.email eq19@users.noreply.github.com
-    #git add . && git commit --allow-empty -m "update params" && git push
-
-    #git clone --single-branch --branch gh-pages $REMOTE_REPO gh-pages && cd gh-pages
-    #git add . && git commit --allow-empty -m "RERUN_RUNNER due to job update" && git push
-    #cd /home/runner && rm -rf /tmp/eq19
-    #exit 1
-  #fi
+  if (( $(echo "$NEW_SCORE > $OLD_SCORE" | bc -l) )); then
+    cat $STRATEGY
+    curl -L -s \
+      -X PATCH \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer $GH_TOKEN" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      "https://api.github.com/repos/$TARGET_REPOSITORY/actions/variables/PARAMS_JSON" \
+      -d "{\"name\":\"PARAMS_JSON\",\"value\":\"$(jq -r '.' $STRATEGY)\"}"
+  fi
 
   #echo -e "\n$hr\nANALYSIS\n$hr"
   #freqtrade backtesting-analysis --help
