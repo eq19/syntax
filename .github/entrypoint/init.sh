0f9b66636d3a4d12c5570471e6b7f504b3985a2f
eq19 <eq19@users.noreply.github.com>
2025-05-19 19:57:25 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -2,12 +2,12 @@
 # Structure: Cell Types – Modulo 6
 # https://www.hexspin.com/proof-of-confinement/
 
-export hr='------------------------------------------------------------------------------------'
+hr='------------------------------------------------------------------------------------'
 
 set_config() {
   echo -e "\n$hr\nCONFIG\n$hr"
   cat /home/runner/work/_actions/eq19/eq19/v2/.github/templates/jekyll_config.yml > $RUNNER_TEMP/_config.yml
-  export PATH=/home/runner/work/_actions/eq19/eq19/v2/.github/entrypoint:$PATH && source artifact.sh
+  export PATH=/home/runner/work/_actions/eq19/eq19/v2/.github/entrypoint:$PATH && bash artifact.sh
 
   cat $RUNNER_TEMP/orgs.json > $1/user_data/ft_client/test_client/results/orgs.json
   gh variable set JEKYLL_CONFIG --body "$(cat $RUNNER_TEMP/_config.yml)"
@@ -23,6 +23,8 @@ set_config() {
   else
     echo "Invalid JSON"
   fi
+
+  echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
 }
 
 git config --global user.name "${GITHUB_ACTOR}"
@@ -62,13 +64,10 @@ if [[ "${JOBS_ID}" == "1" ]]; then
 
   BASE_FOLDER="/home/runner/work/_actions/eq19/eq19/v2/.github"
   if diff -qr ${GITHUB_WORKSPACE}/.github ${BASE_FOLDER} > /dev/null; then set_config $1; fi
-
-  cd ${GITHUB_WORKSPACE} && rm -rf .github
-  cp -r /home/runner/work/_actions/eq19/eq19/v2/.github .
-  chown -R "$(whoami)" .github
+  cd ${GITHUB_WORKSPACE} && rm -rf .github && cp -r ${BASE_FOLDER} . && chown -R "$(whoami)" .github
 
   git remote set-url origin ${REMOTE_REPO}        
-  git add . && git commit -m "update workflows" --quiet && git push --quiet
+  CLEAN_STATUS=$(git add . && git commit -m "update workflows" --quiet && git push --quiet)
 
   if [[ $? -eq 0 ]]; then
 
@@ -94,10 +93,9 @@ if [[ "${JOBS_ID}" == "1" ]]; then
       exit 1
     fi
 
-    echo -e "\n$hr\nENVIRONTMENT\n$hr"
-    printenv | sort
-
+    #Ref: https://github.com/tsoding/JelloVM
     cd $1 && javac -d user_data/ft_client/test_client javaCode/Main.java
+
     cd $GITHUB_WORKSPACE && rm -rf user_data && mv -f $1/user_data .
     echo -e "\n$hr\nWORKSPACE\n$hr" && ls -al .
 
@@ -110,14 +108,12 @@ if [[ "${JOBS_ID}" == "1" ]]; then
 
 elif [[ "${JOBS_ID}" == "2" ]]; then
 
-  ls -alR $GITHUB_WORKSPACE
+  echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
+  echo -e "\n$hr\nWORKSPACE\n$hr" && ls -alR $GITHUB_WORKSPACE
 
   echo -e "\n$hr\nGH BRANCHES\n$hr"
   cd $RUNNER_TEMP && mkdir my-project && cd my-project && git init -q
   git remote add source "$REMOTE_REPO" && git remote add origin "$TARGET_REPO"
-
-  # Get fresh branch lists
-  git fetch --all
 
   # Get list of existing target branches
   existing_target_branches=$(git ls-remote --heads origin | awk -F'/' '{print $3}')
@@ -140,6 +136,7 @@ elif [[ "${JOBS_ID}" == "2" ]]; then
     else
       # Existing branch case
       if [[ "$local_branch" == "gh-pages" ]]; then
+        git fetch origin gh-pages
         # Check if 'docs/' exists in remote
         if ! git ls-tree --name-only "origin/gh-pages" | grep -q "^docs"; then
           echo "No docs/ found - recreating gh-pages"
