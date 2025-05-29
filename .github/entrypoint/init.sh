293f880481f0f7ad0efe1fabb715a288d551d503
eq19 <eq19@users.noreply.github.com>
2025-05-29 01:35:23 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -93,10 +93,14 @@ if [[ "${JOBS_ID}" == "1" ]]; then
       exit 1
     fi
 
-    #Ref: https://github.com/tsoding/JelloVM
-    cd $1 && javac -d user_data/ft_client/test_client javaCode/Main.java
+    cd $GITHUB_WORKSPACE
+    mv -f $1/pythonCode $1/user_data/ft_client/test_client/
+    gcc -Wall -Wextra $1/gccCode/src/decoder.c -o float_decoder
 
-    cd $GITHUB_WORKSPACE && rm -rf user_data && mv -f $1/user_data .
+    #Ref: https://github.com/tsoding/JelloVM
+    javac -d $1/user_data/ft_client/test_client $1/javaCode/Main.java
+
+    rm -rf .dockerignore user_data && mv -f $1/user_data .
     echo -e "\n$hr\nWORKSPACE\n$hr" && ls -al .
 
     # Fetch SHA, encode new content, and update in one step
